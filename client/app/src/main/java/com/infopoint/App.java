40b832766ecc9fd6a7e5bb042fbbe4c159deb7f5
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2023-03-06 22:34:04 +0100

    Added comments

--- client/app/src/main/java/com/infopoint/App.java
+++ client/app/src/main/java/com/infopoint/App.java
@@ -11,6 +11,12 @@ import android.util.Log;
 
 import dagger.hilt.android.HiltAndroidApp;
 
+/*
+    All apps using Hilt must contain an Application class annotated with @HiltAndroidApp.
+    @HiltAndroidApp kicks off the code generation of the Hilt components and also generates a base class
+    for your application that uses those generated components. Because the code generation needs access to all of your modules,
+    the target that compiles your Application class also needs to have all of your Dagger modules in its transitive dependencies.
+*/
 @HiltAndroidApp
 public class App extends Application {
     private static final String _TAG = "[App] ";
