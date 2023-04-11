087df1bd2e34e8cdce84d193fb14c4c63fbffc8d
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2023-04-11 21:45:44 +0200

    Edits

--- server/src/infrastructure/config/main.c
+++ server/src/infrastructure/config/main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv) {
   ini_parser* ini = init_parser(argv[1]); // Parse the given file
-  config* cfg = malloc(sizeof(struct config));
+  //config* cfg = malloc(sizeof(struct config));
 
   /* Retrieve values form the parser */
 
@@ -19,25 +19,31 @@ int main(int argc, char** argv) {
   char* max_threads = (char*) get_value(ini, "connections", "max_threads");
 
   // LOGGING
-  char* log_level = (char*) get_value(ini, "connections", "log_level");
-  char* log_file = (char*) get_value(ini, "connections", "log_file");
+  char* log_level = (char*) get_value(ini, "logging", "log_level");
+  char* log_file = (char*) get_value(ini, "logging", "log_file");
+
+  printf("Network: %s %s %s\n", host, port, timeout);
+
+  printf("Connections: %s %s \n", max_clients, max_threads);
+
+  printf("Logging: %s %s \n", log_level, log_file);
 
   /* Setup values accordingly checking for NULL values */
 
   // Network configuration settings
-  cfg->ns.host = (host != NULL) ? host : "127.0.0.1";
-  cfg->ns.port = (port != NULL) ? port : "8080";
-  cfg->ns.timeout = (timeout != NULL) ? timeout : "0";
+  //cfg->ns.host = (host != NULL) ? host : "127.0.0.1";
+  //cfg->ns.port = (port != NULL) ? port : "8080";
+  //cfg->ns.timeout = (timeout != NULL) ? timeout : "0";
 
-  // Connections configuration settings
-  cfg->cs.max_clients = (max_clients != NULL) ? max_clients : "10";
-  cfg->cs.max_threads = (max_threads != NULL) ? max_threads : "10";
-  
-  // Logging configuration settings
-  cfg->ls.log_level = (log_level != NULL) ? log_level : "normal";
-  cfg->ls.log_file = (log_file != NULL) ? log_file : "stderr";
+  //// Connections configuration settings
+  //cfg->cs.max_clients = (max_clients != NULL) ? max_clients : "10";
+  //cfg->cs.max_threads = (max_threads != NULL) ? max_threads : "10";
+  //
+  //// Logging configuration settings
+  //cfg->ls.log_level = (log_level != NULL) ? log_level : "normal";
+  //cfg->ls.log_file = (log_file != NULL) ? log_file : "stderr";
 
   destroy_parser(ini);
 
-  free(cfg);
+  /* free(cfg); */
 }
