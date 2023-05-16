63a632f1e835ff9dea13cb0dbc31de6270cb091d
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2023-05-16 11:58:57 +0200

    Fixed typo

--- server/src/core/database/database.c
+++ server/src/core/database/database.c
@@ -2,7 +2,7 @@
   ╔╦╗┌─┐┌┬┐┌─┐┌┐ ┌─┐┌─┐┌─┐
    ║║├─┤ │ ├─┤├┴┐├─┤└─┐├┤ 
   ═╩╝┴ ┴ ┴ ┴ ┴└─┘┴ ┴└─┘└─┘
-  Autori:
+  Authors:
   +  Valentino Bocchetti (matr. N86003405)
   +  Dario Morace        (matr. N86003778)
   +  Lucia Brando        (matr. N86003382)
