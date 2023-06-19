e41d929bc8c5060498c2657ad942ee6dc1d44c00
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2023-06-19 15:28:39 +0200

    Updated Search Component

--- client/app/src/main/java/com/infopoint/ui/fragment/SearchFragment.java
+++ client/app/src/main/java/com/infopoint/ui/fragment/SearchFragment.java
@@ -20,26 +20,24 @@
 
 package com.infopoint.ui.fragment;
 
-import static android.content.Context.SEARCH_SERVICE;
-
-import android.app.SearchManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.SearchView;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 
 import com.infopoint.R;
 
 public class SearchFragment extends Fragment {
     private final static String _TAG = "[SearchFragment] ";
-    private SearchView searchView;
-
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
@@ -55,16 +53,28 @@ public class SearchFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle bundle) {
         super.onViewCreated(view, bundle);
-    }
-
-
-    @Override
-    public void onPause() {
-        super.onPause();
+        setupMenu();
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onPause() { super.onPause(); }
+
+    @Override
+    public void onResume() { super.onResume(); }
+
+    private void setupMenu() {
+        requireActivity().addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+                inflater.inflate(R.menu.search_menu, menu);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem item) {
+                Log.d(_TAG, "Item: " + item);
+                return true;
+            }
+        });
+
     }
 }
