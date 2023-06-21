7932e4bde03be6aa25d929a2d5da6b8b12df5d06
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2023-06-21 12:29:04 +0200

    Added logout capabilities with popup confirmation

--- client/app/src/main/java/com/infopoint/ui/fragment/ProfileFragment.java
+++ client/app/src/main/java/com/infopoint/ui/fragment/ProfileFragment.java
@@ -31,16 +31,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.chip.Chip;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.infopoint.R;
+import com.infopoint.core.preferences.StorageManager;
+import com.infopoint.ui.activity.MainActivity;
 
 public class ProfileFragment extends Fragment {
     private final static String _TAG = "[ProfileFragment] ";
-
     private Chip chip;
-    private BottomSheetDialog dialog;
+    private MaterialToolbar toolbar;
 
     @Nullable
     @Override
@@ -54,9 +59,33 @@ public class ProfileFragment extends Fragment {
         super.onViewCreated(view, bundle);
         chip = view.findViewById(R.id.about_us_chip);
         chip.setOnClickListener(click -> showDialog());
+
+        toolbar = view.findViewById(R.id.top_app_bar_profile);
+
+        toolbar.setOnMenuItemClickListener(item -> {
+
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setTitle(R.string.confirm_logout)
+                    .setMessage(R.string.confirm_logout_body)
+                    .setPositiveButton(R.string.confirm_request, (dialogInterface, i) -> {
+                        Log.d(_TAG, "Logout request confirmed");
+                        StorageManager.with(requireContext()).clear();
+                        // TODO: Handle backstack clear
+                        MainActivity main = (MainActivity) requireActivity();
+                        main.navController.navigate(R.id.nav_logout);
+
+                    })
+                    .setNegativeButton(R.string.cancel_request, (dialogInterface, i) -> Log.d(_TAG, "Logout request cancelled"))
+                    .show();
+
+            return false;
+        });
     }
 
+
+    // Custom dialog used to show Authors info & Github links
     private void showDialog() {
+        Log.d(_TAG, "Calling About Us dialog");
         final BottomSheetDialog dialog = new BottomSheetDialog(requireContext());
         dialog.setContentView(R.layout.about_us_modal);
         dialog.findViewById(R.id.about_us_team_card_author_1_github_link).setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/luftmensch-luftmensch/"))));
@@ -68,7 +97,6 @@ public class ProfileFragment extends Fragment {
             intent.putExtra(Intent.EXTRA_EMAIL, "infopoint@info.com");
             requireActivity().startActivity(Intent.createChooser(intent, "Seleziona client di posta elettronica"));
         });
-
         dialog.show();
     }
 }
